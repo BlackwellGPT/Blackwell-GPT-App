fa64dbc3ae80a836f72f9f56544e2d6c54b9e9d2
John O'Meara <johnomeara40@gmail.com>
2023-07-03 12:53:56 -0700

    Add navigation and improve send bar.

---
+++ screens/Conversations.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+
+function Conversations() {
+  return (
+    <StatusBar style="auto" />
+  );
+}
+
+export default Conversations;
---
+++ screens/HowTo.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+
+function HowTo() {
+  return (
+    <StatusBar style="auto" />
+  );
+}
+
+export default HowTo;
