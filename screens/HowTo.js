7f8aa0e1f4b85861afd72f2a8ca649d0cca9eeb4
Ishan Singh <26singhishan@gmail.com>
2023-07-06 13:43:20 -0700

    updated how to guide

--- screens/HowTo.js
+++ screens/HowTo.js
@@ -5,22 +5,19 @@ import {View, Text, StyleSheet, Image} from 'react-native';
 const HowTo = () => {
   return (
     <View>
-      <Text style={styles.textStyle}>HOW-TO</Text>
-      <Text style= {styles.header}>This is a guide on how to use the app to its full capability.</Text>
+      <Text style={styles.title}>HOW-TO</Text>
       <View style ={styles.viewStyle}>
-        <Text style={styles.text}>When you first enter Blackwell GPT, you may feel a little overwhelmed, but fear not!</Text>
-        <Text style={styles.text}>Blackwell is both powerful and capable, yet minimalistic and easy to use. Similar to other AI, you communicate through a chat feature, which accepts both text and voice inputs (powered by Whisper AI).</Text>
-        <View>
-          <Text style={styles.text}>To chat with Blackwell, use the input bar at the bottom of the homepage, which accepts many forms of input.</Text>
-          <Text style ={styles.textStyle}>INPUT DIAGRAM</Text>
+        <Text style={styles.text}>To communicate with Blackwell GPT, use the input bar on the bottom of the home screen.</Text>
+        <Text style={styles.text}>You can use voice, image, and text-based input to chat with Blackwell.</Text>
+        <View style={styles.viewStyleTwo}>
+          <Text style ={styles.secondtitle}>INPUT DIAGRAM</Text>
           <Image 
             style = {styles.imageStyle}
             source={require("../assets/bottombar.jpg")}
           />
-          
         </View>
       </View>
-      
+          
       <StatusBar style="auto" />
     </View>
     
@@ -28,7 +25,7 @@ const HowTo = () => {
 }
 
 const styles = StyleSheet.create({
-  textStyle: {
+  title: {
     padding: 5,
     paddingBottom: 0,
     paddingTop: 20,
@@ -39,27 +36,23 @@ const styles = StyleSheet.create({
     fontWeight: 'bold'
 
   },
-  header: {
-    color: "black",
-    paddingTop: 5,
+  secondtitle: {
     padding: 5,
-    textAlign: "center",
-    backgroundColor: "coral",
-    marginBottom: 0,
-    marginTop: 20,
-    margin: 10,
+    paddingBottom: 0,
+    paddingTop: 20,
+    fontSize: 20,
+    textAlign: "left",
+    color: "coral",
     letterSpacing: 0,
-    overflow: "hidden",
-    borderRadius: 5,
-    fontSize: 10,
-    borderColor: "black",
-    paddingBottom: 5
+    fontWeight: 'bold'
+
   },
   text: {
     letterSpacing: 0,
     padding: 10,
     paddingBottom: 20,
     color: "black",
+    fontSize: 15
 
   },
   viewStyle: {
@@ -72,26 +65,28 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     overflow: 'hidden'
   },
-  imageViewStyle: {
-    borderWidth: 1,
-    margin: 10,
-    borderColor: "black",
-    borderRadius: 10,
-    overflow: 'hidden',
-    alignContent: 'center',
-    borderStyle: 'dashed'
-  },
   imageStyle: {
     width: 300,
     height: 100,
     resizeMode: 'cover',
     aspectRatio: 1.5,
     marginBottom: 20,
-    alignContent: 'center',
     borderRadius: 19,
-    alignSelf: 'center',
-    marginTop: 10
+    alignSelf: 'left',
+    marginTop: 10,
+    margin: 10
   },
-
+  viewStyleTwo: {
+    borderWidth: 1,
+    margin: 10,
+    marginTop: 10,
+    marginBottom: 10,
+    borderColor: "grey",
+    borderStyle: 'dashed',
+    borderRadius: 5,
+    overflow: 'hidden',
+    alignSelf: 'left',
+    padding: 10
+  },
 });
 export default HowTo;
