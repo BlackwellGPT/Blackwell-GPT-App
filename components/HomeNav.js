5c4262df27d937327c7eb9a1ea18f5608903da9f
Ishan Singh <26singhishan@gmail.com>
2023-07-20 12:55:23 -0700

    fixed buttons - aligned to right

--- components/HomeNav.js
+++ components/HomeNav.js
@@ -15,18 +15,18 @@ export default function HomeNav({ navigation }) {
                 <Text style = {styles.textStylethree}>T</Text>
 
 
-                <TouchableOpacity onPress={() => navigation.navigate('Articles')}>
+                <TouchableOpacity style = {styles.coolStyle} onPress={() => navigation.navigate('Articles')}>
                     <Svg width={32} height={32} fill="green" viewBox='0 0 256 256' className="opacity-80">
                         <Path d='M208,24H72A32,32,0,0,0,40,56V224a8,8,0,0,0,8,8H192a8,8,0,0,0,0-16H56a16,16,0,0,1,16-16H208a8,8,0,0,0,8-8V32A8,8,0,0,0,208,24Zm-8,160H72a31.82,31.82,0,0,0-16,4.29V56A16,16,0,0,1,72,40H200Z'></Path>
                     </Svg>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Past Conversations')}>
+                <TouchableOpacity style = {styles.coolStyle} onPress={() => navigation.navigate('Past Conversations')}>
                     <Svg width={32} height={32} fill="green" viewBox='0 0 256 256' className="opacity-80">
                         <Path d='M224,48H32A16,16,0,0,0,16,64V88a16,16,0,0,0,16,16v88a16,16,0,0,0,16,16H208a16,16,0,0,0,16-16V104a16,16,0,0,0,16-16V64A16,16,0,0,0,224,48ZM208,192H48V104H208ZM224,88H32V64H224V88ZM96,136a8,8,0,0,1,8-8h48a8,8,0,0,1,0,16H104A8,8,0,0,1,96,136Z'></Path>
                     </Svg>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => navigation.navigate('Usage Guide')}>
-                    <Svg width={32} height={32} fill="green" viewBox='0 0 256 256' className="opacity-80">
+                <TouchableOpacity style = {styles.coolStyle} onPress={() => navigation.navigate('Usage Guide')}>
+                    <Svg width={32} height ={32} fill="green" viewBox='0 0 256 256' className="opacity-80">
                         <Path d='M140,180a12,12,0,1,1-12-12A12,12,0,0,1,140,180ZM128,72c-22.06,0-40,16.15-40,36v4a8,8,0,0,0,16,0v-4c0-11,10.77-20,24-20s24,9,24,20-10.77,20-24,20a8,8,0,0,0-8,8v8a8,8,0,0,0,16,0v-.72c18.24-3.35,32-17.9,32-35.28C168,88.15,150.06,72,128,72Zm104,56A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z'></Path>
                     </Svg>
                 </TouchableOpacity>
@@ -70,5 +70,9 @@ const styles = StyleSheet.create({
         color: "lime",
         fontWeight: 'bold',
         paddingRight: 18
+    },
+    coolStyle: {
+        alignSelf:'right',
+        
     }
 })
