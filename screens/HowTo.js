29aff437acc8f304c9a03ec0227cef2119989e0d
John O'Meara <johnomeara40@gmail.com>
2023-07-25 16:15:29 -0700

    firebase login/signup

--- screens/HowTo.js
+++ screens/HowTo.js
@@ -1,35 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Text, StyleSheet, View, Image } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function HowTo() {
   return (
-    <View style = {styles.viewStyle}>
-      <Text style = {styles.textStyle}>
+    <View>
+      <Text className="text-lg text-[#333] text-left my-3 p-7 mx-2 mt-7">
         To use Blackwell GPT, click on the text box on the home page and type in your medical-related questions. In the top right of the screen, you can access past conversations, and use the plus button to create a new conversation.
       </Text>
       <StatusBar style="dark" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  textStyle: {
-    // Add your desired styles for the text here
-    fontSize: 16,
-    color: '#333',
-    textAlign: 'left',
-    marginVertical: 10,
-    padding: 30,
-    paddingTop: 10,
-    paddingLeft: 20,
-    marginLeft: 10,
-    marginRight:10,
-    marginTop: 30,
-
-  },
-  viewStyle: {
-    
-  }
-
-});
