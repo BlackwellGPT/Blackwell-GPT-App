015fa54a140ef7d2938675a774e0ecfc0315ace7
KanopyAB <109624256+KanopyAB@users.noreply.github.com>
2023-07-28 11:00:23 -0700

    Logout button
    Added logout button to homenav with icon

--- components/HomeNav.js
+++ components/HomeNav.js
@@ -1,6 +1,7 @@
 import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
 import { Svg, Path } from 'react-native-svg'
 import * as NavigationBar from 'expo-navigation-bar'
+import Login from '../screens/Login'
 
 NavigationBar.setBackgroundColorAsync('#fff')
 NavigationBar.setButtonStyleAsync('dark')
@@ -49,6 +50,17 @@ export default function HomeNav ({ navigation }) {
           >
             <Path d='M140,180a12,12,0,1,1-12-12A12,12,0,0,1,140,180ZM128,72c-22.06,0-40,16.15-40,36v4a8,8,0,0,0,16,0v-4c0-11,10.77-20,24-20s24,9,24,20-10.77,20-24,20a8,8,0,0,0-8,8v8a8,8,0,0,0,16,0v-.72c18.24-3.35,32-17.9,32-35.28C168,88.15,150.06,72,128,72Zm104,56A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z'></Path>
           </Svg>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Svg  
+            width={32}
+            height={32}
+            viewBox='0 0 256 256'
+            strokeWidth={2}
+            className='fill-blue-500 stroke-blue-500'
+            >
+              <Path d='M112,216a8,8,0,0,1-8,8H48a16,16,0,0,1-16-16V48A16,16,0,0,1,48,32h56a8,8,0,0,1,0,16H48V208h56A8,8,0,0,1,112,216Zm109.66-93.66-40-40a8,8,0,0,0-11.32,11.32L196.69,120H104a8,8,0,0,0,0,16h92.69l-26.35,26.34a8,8,0,0,0,11.32,11.32l40-40A8,8,0,0,0,221.66,122.34Z'></Path>
+            </Svg>
         </TouchableOpacity>
       </View>
     </View>
