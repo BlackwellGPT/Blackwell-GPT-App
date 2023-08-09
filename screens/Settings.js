5864460278d4a19713a511b08b5c16b64e12216e
Ishan Singh <26singhishan@gmail.com>
2023-08-09 11:10:36 -0700

    icon changes settings.js

--- screens/Settings.js
+++ screens/Settings.js
@@ -11,11 +11,11 @@ export default function Settings({navigation}) {
                 <Text style = {styles.textStyle}>Log Out</Text>
                 <TouchableOpacity onPress={() => navigation.navigate('Login')}>
                 <Svg  
-                    width={32}
-                    height={32}
+                    width={20}
+                    height={20}
                     viewBox='0 0 256 256'
                     strokeWidth={2}
-                    className='fill-gray-500 stroke-blue-500'
+                    className='fill-green-500 stroke-blue-500'
                     >
                     <Path d='M112,216a8,8,0,0,1-8,8H48a16,16,0,0,1-16-16V48A16,16,0,0,1,48,32h56a8,8,0,0,1,0,16H48V208h56A8,8,0,0,1,112,216Zm109.66-93.66-40-40a8,8,0,0,0-11.32,11.32L196.69,120H104a8,8,0,0,0,0,16h92.69l-26.35,26.34a8,8,0,0,0,11.32,11.32l40-40A8,8,0,0,0,221.66,122.34Z'></Path>
                     </Svg>
@@ -39,7 +39,7 @@ const styles = StyleSheet.create({
     },
     textStyle: {
         padding: 0,
-        fontSize: 20,
+        fontSize: 15,
         fontWeight: 'bold'
     },
     header: {
